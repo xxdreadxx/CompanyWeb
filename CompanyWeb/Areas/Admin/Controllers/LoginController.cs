4eed65a9c9c157516130ea8eb23a6e7c466570ac
WINDOWS-11\Administrator <theend.spirit@gmail.com>
2023-12-23 17:11:06 +0700

    commit

---
+++ CompanyWeb/Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class LoginController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
